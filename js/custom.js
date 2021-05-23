20d88204ef1e98b8519e90c3a9693e184a4a7a0e
Abdelrahmanman <Abdelrahman.man@hotmail.com>
2021-05-23 18:19:08 +0200

    Settings Box

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,14 @@ for(let x = 0; x < 5; x++) {
 landingPage.style.backgroundImage = "url('images/slide0.jpg')";
 
 setInterval(function() {
-    let randomNum = Math.floor(Math.random() * this.images.length);
-    this.landingPage.style.backgroundImage = 'url("images/'+ images[randomNum] +'.jpg")'
-}, 5000);
+    let randomNum = Math.floor(Math.random() * (this.images.length - 1));
+    this.landingPage.style.backgroundImage = 'url("images/'+ images[randomNum] +'.jpg")';
+}, 10000);
+
+var gearIcon = document.querySelector(".settings-box .fa-gear");
+
+function doToggle() {
+    var settingBox = document.querySelector(".settings-box");
+    settingBox.classList.toggle('opened');
+    document.querySelector(".settings-box .fa-gear").classList.toggle("fa-spin");
+}
