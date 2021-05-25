d233d0431396d56e148a58ed793198504dd4b060
Abdelrahmanman <Abdelrahman.man@hotmail.com>
2021-05-25 12:40:53 +0200

    Toggle colors in settings box

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,13 @@
+//Check If there's a local storage color option
+let mainColor = localStorage.getItem("color_option");
+if (mainColor != null) {
+    // set color
+    document.documentElement.style.setProperty("--main--color", mainColor);
+}
+
+
+
+// Start Slider
 // Select Landing page element
 var landingPage = document.querySelector(".landing-page");
 // get array of images
@@ -12,6 +22,9 @@ setInterval(function() {
     this.landingPage.style.backgroundImage = 'url("images/'+ images[randomNum] +'.jpg")';
 }, 10000);
 
+// End slider
+
+// Toggle Settings 
 var gearIcon = document.querySelector(".settings-box .fa-gear");
 
 function doToggle() {
@@ -19,3 +32,24 @@ function doToggle() {
     settingBox.classList.toggle('opened');
     document.querySelector(".settings-box .fa-gear").classList.toggle("fa-spin");
 }
+
+// switch colors
+// get all colors
+const colorsLi = document.querySelectorAll(".color-list li");
+colorsLi.forEach(element => {
+    // add event to element
+    element.addEventListener("click", (e)=> {
+        // remove all active class in another siblings
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active")
+        });
+        // add active class to element
+        e.target.classList.add("active");
+        // set color on root
+        document.documentElement.style.setProperty("--main--color", e.target.dataset.color);
+        // set item into local storage
+        localStorage.setItem("color_option", e.target.dataset.color);
+    }); 
+});
+
+// End Toggle Settings
